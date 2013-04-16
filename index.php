79abd1ac39f9c01652971724e51672ec1eec77cc
Robin Harteveld <robin.harteveld@gmail.com>
2013-04-16 15:54:51 +0200

    vzfgzf

--- index.php
+++ index.php
@@ -4,11 +4,16 @@ mysql_connect("eu-cdbr-azure-north-a.cloudapp.net", "b8563c94b46c37", "f673e89a"
 mysql_select_db("idpskool01") or die(mysql_error());
 
 // Retrieve all the data from the "user" table
-$result = mysql_query("SELECT username FROM users")
+$result = mysql_query("SELECT username,skoolid FROM users")
 or die(mysql_error());  
 
 // store the record of the "example" table into $row
-$row = mysql_fetch_array( $result );
+////$row = mysql_fetch_array( $result );
 // Print out the contents of the entry 
-var_dump($row);
+////var_dump($row);
+while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+    printf("ID: %s  Name: %s", $row[0], $row[1]);
+}
 
+mysql_free_result($result);
+?>
